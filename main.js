d1f2180c2ae9493a3b622d0aa53ef6891fb32d51
AufaZuhd <aufasyakir1@gmail.com>
2025-12-12 14:08:23 +0700

    feat : money logic

--- main.js
+++ main.js
@@ -6,4 +6,12 @@ const umur = Number(input("Berapa umurmu? "));
 if (umur<21){
     const umurKurang = 21 - umur;
     console.log(`Maaf ${nama}, umur kamu kurang ${umurKurang} tahun lagi`);
+} else {
+    let uang = Number(input("Berapa uang yang kamu bawa? "));
+    if (uang<500000){
+        uang = uang/1000;
+        console.log(`Maaf ${nama}, uang kamu cuma ${uang} ribu. Datang lagi lain kali`);
+    } else {
+        console.log(`Selamat datang ${nama}`);
+    }
 }
